c998ae5546dc6db9350a8cb76cb9770802e556cd
nohtyp0202 <accutesting19@gmail.com>
2020-06-27 17:08:51 -0500

    In this repository actions that can be performed on file are included: 1. new file creation and opening 2. opening file to write data 3. Mandatory closing of the file 4. opening a file to read the data from the file 5. tell() -- to know the current position of the file 6. seek() -- to point the cursor to the specified position

--- ReadingFromFile.py
+++ ReadingFromFile.py
@@ -17,3 +17,17 @@ with (open("FileReading.txt","r")) as re:
    print re.read() # if no parameter is passed then it will read till end of the document
 
 print re.closed # As with..as is used files are always closed in the end
+
+
+# tell() --> tells the position of the cursor in the file
+
+with (open("FileReading.txt", "r")) as telfil:
+    print telfil.tell() # tells the current position of the cursor in the file
+
+
+# seek() --> seek(<parameter_int_intended position of cursor>) method takes the cursor to the specified position
+
+with (open("FileReading.txt","r")) as refil:
+    refil.seek(20) # Cursor is now pointing to the 20th character in the file
+    print refil.read(15) # starting from 20th character it will read NEXT 15 character
+    print refil.tell() # tells the new position of the cursor ( 20 + 15 = 35 )
